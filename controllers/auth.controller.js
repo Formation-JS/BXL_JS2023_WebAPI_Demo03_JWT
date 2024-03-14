b3b7e92ef1857e06ebd468540a016e21103b83aa
Aurélien <aurelien.strimelle@bstorm.be>
2024-03-14 11:53:57 +0100

    Implemente login simple 🤨

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,32 @@
+import memberService from '../services/member.service.js';
+
 const authController = {
 
     login: async (req, res) => {
-        res.sendStatus(501);
+        const data = req.body;
+
+        // Validation
+        if(!data || !data.username || !data.password) {
+            res.status(422)
+               .json({
+                    errorMessage: 'Invalide data'
+               });
+            return;
+        }
+
+        // Login via le service
+        const member = memberService.login(data.username, data.password);
+
+        if(!member) {
+            res.status(400)
+               .json({
+                    errorMessage: 'Invalide credential'
+               });
+            return;
+        }
+
+        // TODO Create et send a JWT !
+        res.sendStatus(204);
     }
 }
 
